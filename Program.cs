2cd8d45e750b7b4f1e59d06db6d2b9671d98d25e
LILI <LILI@LILIENCHEVA>
2024-12-11 18:39:28 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using homework11_12;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Shape[] shapes = new Shape[]
+        {
+            new Circle(),
+            new Rectangle(),
+            new Triangle()
+        };
+
+        // Извикване на метода Draw за всяка фигура чрез полиморфизъм
+        foreach (Shape shape in shapes)
+        {
+            shape.Draw();
+        }
+    }
+}
---
+++ Rectangle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework11_12
+{
+    class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing a Rectangle.");
+        }
+    }
+}
